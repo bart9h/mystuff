805d57502f1597bca0c5388250224f10d233cdd2
rodolfo.borges <rodolfo.borges@6d5da786-ff2f-0410-aed1-cf8bb357d6a5>
2007-11-25 14:55:22 +0000

    folding, "description" header
    git-svn-id: https://bart9h-stuff.googlecode.com/svn/trunk@48 6d5da786-ff2f-0410-aed1-cf8bb357d6a5

--- scripts/dif.sh
+++ scripts/dif.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+#{{{1 description
+#
+#  This is a frontend for diff.
+#  Usage:   dif <object1> <object2>
+#  where the objects can be a file, a directory, or a tar archive.
+#  Uses vim to view the output, if available.
+#
 
-##########
-# configurable variables
+#{{{1 configurable variables
 
 diff='/usr/bin/diff -U 5 -rpbB --ignore-all-space --ignore-blank-lines --exclude=.svn --exclude=CVS'
 
 
-##########
-# handle arguments
+#{{{1 argument handling
 
 GVIM=0
 if test "$1" == "-g"; then
@@ -17,6 +22,7 @@ fi
 
 verbose=
 test "$VERBOSE" -a "$VERBOSE" != "no" -a "$VERBOSE" != "0" && verbose='-v'
+#TODO: function bool() {}
 
 nargs=$#
 test $nargs -eq 2 || { echo 'use: dif <object1> <object2>'; exit; }
@@ -56,16 +62,14 @@ while test "$1"; do
 done
 
 
-##########
-# do the diff
+#{{{1 do the diff
 
 output=/tmp/dif
 echo "$diff \"$a\" \"$b\""
 $diff "$a" "$b" | sed "s/\r//" > $output
 
 
-##########
-# view the output
+#{{{1 view the output
 
 if test -s $output; then
 	if test -x "$GVIM"; then
@@ -84,11 +88,13 @@ if test -s $output; then
 fi
 
 
-##########
-# cleanup temporary data
+#{{{1 cleanup temporary data
 
 test -n "$verbose" && echo "cleaning up..."
 test "$adir" && rm $verbose -rf "$adir"
 test "$bdir" && rm $verbose -rf "$bdir"
 rm $verbose -rf $output
 
+
+#}}}
+# vim600:fdm=marker:
