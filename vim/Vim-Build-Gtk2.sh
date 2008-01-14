f0acdc69a377b14fe166f79ab600c85b1629e052
rodolfo.borges <rodolfo.borges@6d5da786-ff2f-0410-aed1-cf8bb357d6a5>
2008-01-14 19:57:56 +0000

    unified build scripts into one
    git-svn-id: https://bart9h-stuff.googlecode.com/svn/trunk@82 6d5da786-ff2f-0410-aed1-cf8bb357d6a5

--- vim/Vim-Build-Gtk2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-./configure \
-	--enable-perlinterp \
-	--enable-gui=gtk2 \
-	--disable-nls \
-&& make clean && nice make -j2  && ldd src/vim
---
+++ vim/build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+case "$1" in
+	''|'text')
+		./configure \
+			--prefix=$PREFIX \
+			--disable-nls \
+			--enable-multibyte \
+			--enable-perlinterp \
+			--enable-gui=no \
+			--without-x \
+			|| exit
+	;;
+	'gui')
+		./configure \
+			--prefix=$PREFIX \
+			--disable-nls \
+			--enable-multibyte \
+			--enable-perlinterp \
+			--enable-gui=gtk2 \
+			|| exit
+	;;
+	*)
+		echo '?'
+	;;
+esac
+
+make clean && nice make -j2  && ldd src/vim
+
