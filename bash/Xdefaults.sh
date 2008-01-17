ed0ff741187c59c44864186c0f96eb9c952145d8
rodolfo.borges <rodolfo.borges@6d5da786-ff2f-0410-aed1-cf8bb357d6a5>
2008-01-17 10:45:14 +0000

    different font sizes for different hosts
    git-svn-id: https://bart9h-stuff.googlecode.com/svn/trunk@83 6d5da786-ff2f-0410-aed1-cf8bb357d6a5

--- bash/Xdefaults.sh
+++ bash/Xdefaults.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# xrdb don't accept '#' comments anymore
-sed \
-		';s/^[\ \t]\+//;/^\#/d' \
-		"$HOME/src/etc/Xdefaults" \
-| xrdb
+case "$HOSTNAME" in
+	doti-lap)
+		hostfile='small'
+		;;
+	*)
+		hostfile='default'
+		;;
+esac
+
+test -n "$hostfile" &&
+files="$HOME/src/etc/Xdefaults.$hostfile"
+files="$HOME/src/etc/Xdefaults $files"
+cat $files | sed ';s/^[\ \t]\+//;/^\#/d' | xrdb
 
