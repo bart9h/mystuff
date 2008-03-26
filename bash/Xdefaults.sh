83ddef4b85d7dfd4d5a903428a3e3aa966823ea1
rodolfo.borges <rodolfo.borges@6d5da786-ff2f-0410-aed1-cf8bb357d6a5>
2008-03-26 02:51:51 +0000

    s/src/etc/
    git-svn-id: https://bart9h-stuff.googlecode.com/svn/trunk@120 6d5da786-ff2f-0410-aed1-cf8bb357d6a5

--- bash/Xdefaults.sh
+++ bash/Xdefaults.sh
@@ -10,7 +10,7 @@ case "$HOSTNAME" in
 esac
 
 test -n "$hostfile" &&
-files="$HOME/src/etc/Xdefaults.$hostfile"
-files="$HOME/src/etc/Xdefaults $files"
+files="$HOME/etc/etc/Xdefaults.$hostfile"
+files="$HOME/etc/etc/Xdefaults $files"
 cat $files | sed ';s/^[\ \t]\+//;/^\#/d' | xrdb
 
