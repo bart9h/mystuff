b50c2e9a5c6a73f5258dfc2c29738327a0e5a057
rodolfo.borges <rodolfo.borges@6d5da786-ff2f-0410-aed1-cf8bb357d6a5>
2008-05-18 22:39:57 +0000

    minor improvements on qiv-command.sh
    - 1,2,3 easy to remember (hopefully) keys for help,edit,run
    - qiv-action can be any executable type (called with exec, not sh)
    
    
    git-svn-id: https://bart9h-stuff.googlecode.com/svn/trunk@139 6d5da786-ff2f-0410-aed1-cf8bb357d6a5

--- desktop/qiv-command.sh
+++ desktop/qiv-command.sh
@@ -1,15 +1,31 @@
 #!/bin/sh
+#####################################################################
+#
+#    Keys that can be used from "qiv":
+#
+#    1    show this help
+#    2    edit the script
+#    3    execute the script, passing current file as first arg
+#
+#####################################################################
+#{{{ code
 
 key="$1"
 file="$2"
-action_script="$HOME/var/qiv-action.sh"
+action_script="$HOME/var/qiv-action"
 
 case "$key" in
-0)
-	sh "$action_script" "$file"
+1)
+	xterm -e "vi \"$0\""
 	;;
-7)
+2)
 	xterm -e "vi \"$action_script\""
+	;;
+3)
+	chmod +x "$file" &&
+	exec "$action_script" "$file"
 	;;
 esac
 
+#}}}
+# vim600:fdm=marker:
