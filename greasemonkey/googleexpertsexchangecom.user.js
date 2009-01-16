d6fd45dce466c0da0b5188943cbfcc3d9e15ff97
Rodolfo Borges <rodolfo.borges@gmail.com>
2009-01-16 11:00:19 -0200

    fix do bogado pro googleexpertexchange

--- greasemonkey/googleexpertsexchangecom.user.js
+++ greasemonkey/googleexpertsexchangecom.user.js
@@ -3,14 +3,24 @@
 // @namespace     http://www.google.com
 // @description	  Removes results for any "experts-exchange.com" page from google.
 // @include       http://www.google.com/search*
+// @include       http://www.google.com.br/search*
 // @include       http://www.google.co.uk/search*
 // ==/UserScript==
 
 // Hacked from arantius' "Google about.com remover"
 //   http://www.arantius.com/misc/greasemonkey/
 
-var results=document.evaluate('//a[contains(@href, "experts-exchange.com/")]/..', 
-	document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-for (var result=null, i=0; result=results.snapshotItem(i); i++) {
-	result.style.display='none';
+var forbiden = [ 
+	'experts-exchange.com/' ];
+
+for each (var i in forbiden)
+{
+	var results=document.evaluate(
+		'//a[contains(@href, "' + i + '")]/../..', 
+		document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+
+	for (var result=null, i=0; result=results.snapshotItem(i); i++)
+	{
+		result.style.display='none';
+	}
 }
