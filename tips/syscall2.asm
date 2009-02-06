f302b61eac4d930ed70b56df5234a7f8d928a796
Rodolfo Borges <rodolfo.borges@gmail.com>
2009-02-06 09:20:48 -0200

    comments for asm helloworld

--- tips/syscall2.asm
+++ tips/syscall2.asm
@@ -4,13 +4,13 @@ entry _start
 segment readable executable
 
 _start:
-mov     al,     4
-mov     bl,     1
+mov     al,     4       ;4=write, see  http://docs.cs.up.ac.za/programming/asm/derick_tut/syscalls.html
+mov     bl,     1       ;1=stdout
 mov     ecx,    message
 mov     dl,     messageLen
 int     0x80
 
-mov     al,     1
+mov     al,     1       ;1=exit
 mov     bl,     0
 int     0x80
 
