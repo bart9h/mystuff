595534d941cd7b10d79eeb592c9733c5312cdc86
Rodolfo Borges <rodolfo.borges@gmail.com>
2011-05-04 20:41:39 -0300

    job function with no args lists current jobs

--- bash/job.sh
+++ bash/job.sh
@@ -4,12 +4,19 @@ function job()
 {
 	local spool_dir="/tmp/jobs"
 
+	if test "$1" == "wait"; then
+		while test $( ls "$spool_dir" | wc -l ) -ge 1; do
+			sleep 1s
+		done
+		return
+	fi
+
 	if test -z "$*"; then
 		ls "$spool_dir"
 		return
 	fi
 
-	local max_jobs=`show cpus`
+	local max_jobs=$( show cpus )
 
 	test -d "$spool_dir" || mkdir "$spool_dir" || exit
 
