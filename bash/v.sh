b7ef2c43484af88049a81181aa9f874943f55e80
Rodolfo Borges <rodolfo.borges@gmail.com>
2016-03-22 11:30:34 -0300

    v.sh: keep pwd when opening modified files

--- bash/v.sh
+++ bash/v.sh
@@ -6,11 +6,12 @@ function v()
 		vim -o "$@"
 	elif git rev-parse --git-dir >/dev/null 2>&1 && git status --porcelain --untracked-files=no | grep '^.M' >/dev/null; then
 		(
+			pwd="$(sed 's/\ /\\\ /g' <<< $PWD)"
 			cd "$(git rev-parse --git-dir)/.." &&
 			git status --porcelain --untracked-files=no |
 				grep '^.M' |
 				cut -c 4- |
-				exec xargs -d '\n' $ETC/bash/vim_tty -o
+				exec xargs -d '\n' $ETC/bash/vim_tty +"chdir $pwd" -o
 		)
 	elif find -maxdepth 1 -type f | grep . >/dev/null 2>&1; then
 		vim "$(find -maxdepth 1 -type f -not -name '.*' -print0 | xargs -0 ls -1rt | tail -1)"
