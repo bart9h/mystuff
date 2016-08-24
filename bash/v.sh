1d6ba7f7d5e9b30f1c053d4090f611c036183e2d
Rodolfo Borges <rodolfo.borges@gmail.com>
2016-08-24 12:50:08 -0300

    v.sh without args only edit text files

--- bash/v.sh
+++ bash/v.sh
@@ -4,7 +4,9 @@ function v()
 {
 	if test "$1"; then
 		vim -o "$@"
-	elif git rev-parse --git-dir >/dev/null 2>&1 && git status --porcelain --untracked-files=no | grep '^.M' >/dev/null; then
+	elif git rev-parse --git-dir >/dev/null 2>&1 \
+	&&   git status --porcelain --untracked-files=no | grep '^.M' >/dev/null
+	then
 		(
 			pwd="$(sed 's/\ /\\\ /g' <<< $PWD)"
 			cd "$(git rev-parse --git-dir)/.." &&
@@ -14,7 +16,14 @@ function v()
 				exec xargs -d '\n' $ETC/bash/vim_tty +"chdir $pwd" -o
 		)
 	elif find -maxdepth 1 -type f | grep . >/dev/null 2>&1; then
-		vim "$(find -maxdepth 1 -type f -not -name '.*' -print0 | xargs -0 ls -1rt | tail -1)"
+		vim "$(
+			find -maxdepth 1 -type f -not -name '.*' -print0 |
+			xargs -0 file |
+			grep ':.*\<text\>' |
+			cut -d : -f 1 |
+			xargs ls -1t |
+			head -1
+		)"
 	else
 		vim .
 	fi
