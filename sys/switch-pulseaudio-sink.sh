b1d52dd232d03d30996f2c7b6f69b24387e91488
Rodolfo Borges <rodolfo.borges@gmail.com>
2017-07-25 13:32:49 -0300

    switch-pulseaudio-sink: need to move sink input
    Now it switchs currently-playing audio correctly.

--- sys/switch-pulseaudio-sink.sh
+++ sys/switch-pulseaudio-sink.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
 dir="$HOME/.cache/switch-pulseaudio-sink"
+sink_input="$( pacmd list-sink-inputs | grep 'index:' | head -1 | cut -d : -f 2 )"
+sink_output=
+
 case "$(cat "$dir/current.index")" in
 	1)
-		pacmd set-default-sink 0
-		echo 0 > "$dir/current.index"
+		sink_output=0
 		ln -s -f "/usr/share/icons/mate/48x48/devices/audio-card.png" "$dir/current.png"
 		;;
 	*)
-		pacmd set-default-sink 1
-		echo 1 > "$dir/current.index"
+		sink_output=1
 		ln -s -f "/usr/share/icons/mate/48x48/devices/video-display.png" "$dir/current.png"
 		;;
 esac
+
+echo "$sink_output" > "$dir/current.index"
+pacmd 'set-default-sink' "$sink_output"
+test -n "$sink_input" && pacmd 'move-sink-input' "$sink_input" "$sink_output"
