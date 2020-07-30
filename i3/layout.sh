2b3309155c75d7e6773bfaea7508a0387e9e367b
Rodolfo Borges <rodolfo.borges@gmail.com>
2020-07-30 04:01:53 -0300

    i3 key for skel layout

---
+++ i3/layout.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+layout() {
+	i3-msg 'exec mate-terminal'
+	sleep .1
+	i3-msg 'exec mate-terminal'
+	sleep .1
+	i3-msg 'resize grow width 20 px or 20 ppt'
+	i3-msg 'split v'
+	i3-msg 'focus left'
+	i3-msg 'split v'
+	i3-msg 'focus right'
+	i3-msg 'layout tabbed'
+}
+
+layout
+#i3-msg workspace 2
+#layout
+#i3-msg workspace 3
+#layout
+#i3-msg workspace 1
