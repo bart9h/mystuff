9a2be2629c96576d93bbf3b5622638c718b3d36d
Rodolfo Borges <rodolfo.borges@gmail.com>
2020-08-17 03:38:38 -0300

    i3 hotkey to select and copy a clip

---
+++ util/clippaste.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+if test "$1" == ""; then
+	exec mate-terminal -x "$0" "${ETC}/tips/clips"
+fi
+
+readarray -t lines < "${ETC}/tips/clips"
+count=${#lines[@]}
+cursor=0
+done=0
+key=
+while test "$done" == "0"; do
+	clear
+	for i in `seq 0 $(( $count - 1 ))`; do
+		if test "$i" == "$cursor"; then echo -n '>>  '; else echo -n '    '; fi
+		echo "${lines[$i]}"
+	done
+	read -s -N 1 key
+	case "$key" in
+		j)
+			cursor=$(( $cursor + 1 ))
+			;;
+		k)
+			cursor=$(( $cursor - 1 ))
+			;;
+		l)
+			echo -n "${lines[$cursor]}" | xsel -i -b
+			done=1
+			;;
+		h|q|)
+			done=1
+			;;
+	esac
+	if test "$cursor" -lt 0; then cursor=0; fi
+	if test "$cursor" -ge $count; then cursor=$(( $count - 1 )); fi
+done
