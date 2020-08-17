f0e693a837158af8344de95e3867c37f172af8b6
Rodolfo Borges <rodolfo.borges@gmail.com>
2020-08-17 13:49:07 -0300

    clippaste remembers cursor position

--- util/clippaste.sh
+++ util/clippaste.sh
@@ -5,7 +5,8 @@ fi
 
 readarray -t lines < "${ETC}/tips/clips"
 count=${#lines[@]}
-cursor=0
+cursor="$(cat /tmp/clippaste-cursor)"
+test -z "$cursor" && cursor=0
 done=0
 key=
 while test "$done" == "0"; do
@@ -33,3 +34,4 @@ while test "$done" == "0"; do
 	if test "$cursor" -lt 0; then cursor=0; fi
 	if test "$cursor" -ge $count; then cursor=$(( $count - 1 )); fi
 done
+echo $cursor >| /tmp/clippaste-cursor
