ca213773567c9962a51a6189f949933664f5f557
Rodolfo Borges <rodolfo.borges@gmail.com>
2020-08-17 14:19:02 -0300

    clippaste cursor wraps

--- util/clippaste.sh
+++ util/clippaste.sh
@@ -31,7 +31,7 @@ while test "$done" == "0"; do
 			done=1
 			;;
 	esac
-	if test "$cursor" -lt 0; then cursor=0; fi
-	if test "$cursor" -ge $count; then cursor=$(( $count - 1 )); fi
+	if test "$cursor" -lt 0; then cursor=$(( $count - 1 )); fi
+	if test "$cursor" -ge $count; then cursor=0; fi
 done
 echo $cursor >| /tmp/clippaste-cursor
