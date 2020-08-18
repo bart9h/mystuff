f854d679717adbda8928e4e80a4874916e87684b
Rodolfo Borges <rodolfo.borges@gmail.com>
2020-08-18 03:41:35 -0300

    baixa fotos de varias pastas do celular

--- scripts/baixar_fotos_celular.pl
+++ scripts/baixar_fotos_celular.pl
@@ -3,21 +3,25 @@ use strict;
 use warnings;
 use v5.10;
 
+my $ultima_baixada = '20200207';
+
 # find ~/fotos/ -name '*.jpg' | sort | xargs exiftool -Model -DateTimeOriginal | less
 
 #my $source_path = '/sdcard/DCIM/Camera';
-my $source_path = '/storage/3930-6232/DCIM/Camera';
-my $ultima_baixada = '20200207';
+my @source_path = ( '/storage/3930-6232/DCIM/Camera', '/storage/self/primary/DCIM/Camera' );
 
 my @files;
-foreach (`adb shell ls $source_path`) {
-	chomp;
-	m{((?:IMG|VID)_(\d\d\d\d\d\d\d\d)_.*)} or next;
-	my ($file, $data) = ($1, $2);
-	next if $data <= $ultima_baixada;
-	push @files, $source_path.'/'.$file;
+foreach my $source_path (@source_path) {
+	foreach (`adb shell ls $source_path`) {
+		chomp;
+		m{((?:IMG|VID)_(\d\d\d\d\d\d\d\d)_.*)} or next;
+		my ($file, $data) = ($1, $2);
+		next if $data <= $ultima_baixada;
+		push @files, $source_path.'/'.$file;
+	}
 }
 
 my $cmd = 'sudo adb pull '.join(' ', @files).' .';
 say $cmd;
 system $cmd;
+say "NAO ESQUECA DE ATUALIZAR A DATA DA ULTIMA FOTO BAIXADA!";
