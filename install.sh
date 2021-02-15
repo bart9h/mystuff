2443e675324776a51aa783df40b114196b54e0ab
Rodolfo Borges <rodolfo.borges@gmail.com>
2021-02-15 05:34:58 -0300

    git config in $XDG_CONFIG_HOME

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 cd "$HOME"  || exit 1
 
+test -z "$XDG_CONFIG_HOME" && XDG_CONFIG_HOME="$HOME/.config"
+
 test -z "$ETC" && ETC="$HOME/etc"
 if ! test -d "$ETC"; then
 	echo "no directory \"$ETC\""
@@ -15,9 +17,9 @@ for cmd in git vim; do
 	fi
 done
 
-function L() {
-	local from="$ETC/$1"
-	local to="$HOME/$2"
+function LINK() {
+	local from="$1"
+	local to="$2"
 
 	if test -e "$to"; then
 		if test "`readlink "$to"`" == "$from"; then
@@ -34,14 +36,21 @@ function L() {
 	ln -sv "$from" "$to"  || exit 1
 }
 
+function L() {
+	LINK "$ETC/$1" "$HOME/$2"
+}
+
 L bash/bashrc    .bashrc
-L etc/gitconfig  .gitconfig
 L etc/inputrc    .inputrc
 L mplayer        .mplayer
 L etc/screenrc   .screenrc
 L etc/tigrc      .tigrc
 L vim            .vim
 L x11/xsession   .xsession
+
+git_dir="$XDG_CONFIG_HOME/git"
+test -d "$git_dir" || mkdir -v -p "$git_dir"
+LINK "$ETC/etc/gitconfig"  "$git_dir/config"
 
 vundle_dir="$HOME/.vim/bundle/vundle"
 if test -e "$vundle_dir"; then
