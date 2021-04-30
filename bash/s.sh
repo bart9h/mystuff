ffe85e06da936793d78d3abff04df93ef9819fc6
Rodolfo Borges <rodolfo.borges@gmail.com>
2021-04-30 17:29:33 -0300

    fix s.sh conditionals, now zsh compatible

--- bash/s.sh
+++ bash/s.sh
@@ -4,7 +4,7 @@ function s()
 {
 	which tmux >/dev/null || return
 
-	if test "$1" == "--help" -o "$1" == "-h"; then
+	if [[ "$1" == "--help" || "$1" == "-h" ]]; then
 		cat <<EOF
 (no args)       list existing tmux sessions
 name            attach to named session if it exists, otherwise creates
@@ -16,7 +16,7 @@ If name is an existing directory, chdir into it and starts tmux there.
 If directory ends in "src" or "build", use parent dir as label (no chdir is done).
 EOF
 		return
-	elif test "$1" == ""; then
+	elif [[ "$1" == "" ]]; then
 		tmux list-sessions
 		return
 	fi
@@ -25,39 +25,39 @@ EOF
 	local escape=""
 
 	# if arg is _, create "meta" tmux session
-	if test "$name" == "_"; then
+	if [[ "$name" == "_" ]]; then
 		escape="set-option -g prefix C-s \; "
-		if test -n "$1"; then
+		if [[ -n "$1" ]]; then
 			name="$1"; shift
 		fi
 	fi
 
 	local is_dot=0
-	if test "$name" == "."; then
+	if [[ "$name" == "." ]]; then
 		is_dot=1
 		name="$(basename "$PWD")"
 	fi
 
 	# if arg is existing session, attach
-	if test -z "$1" && tmux list-sessions -F '#{session_name}' | grep "^${name}$"; then
+	if [[ -z "$1" ]] && tmux list-sessions -F '#{session_name}' | grep "^${name}$"; then
 		tmux attach-session -t $name
 
 	else  # create session
 		local session_name
 
 		# if arg is existing dir, cd into, else arg is session name
-		test $is_dot == 0 -a -d "$name" && cd "$name" || session_name="$name"
+		[[ $is_dot == 0 && -d "$name" ]] && cd "$name" || session_name="$name"
 
 		# defult session name is $PWD
-		test -z "$session_name" -o "$session_name" == "." \
+		[[ -z "$session_name" || "$session_name" == "." ]] \
 		&& session_name="`basename "$PWD"`"
 
-		if test "$session_name" == "src"; then
+		if [[ "$session_name" == "src" ]]; then
 			local p="$(cd .. && basename "$PWD")"
-			test "$p" != "/" && session_name="$p"
+			[[ "$p" != "/" ]] && session_name="$p"
 		fi
 
-		if -n "$escape"; then
+		if [[ -n "$escape" ]]; then
 			tmux set-option -g prefix C-s \; new-session -s "$session_name"
 		else
 			tmux new-session -s "$session_name"
